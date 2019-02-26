2b7d1c01a67f0da25a40a20e74c8f020a691009e
MichalKar <michkarkowski@vp.pl>
2019-02-26 13:14:30 +0100

    Initial commit

---
+++ src/main/java/com/bdii/BdiiProject/BdiiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bdii.BdiiProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdiiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdiiProjectApplication.class, args);
+	}
+
+}
